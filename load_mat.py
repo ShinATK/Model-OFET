545da4fc1193a5b67a448c3decc05c41d54d02e8
Shin <iakameiloveyou@gmail.com>
2021-11-14 16:42:55 +0800

    Update 2021-11-14

--- load_mat.py
+++ load_mat.py
@@ -1,6 +1,8 @@
 from scipy.io import loadmat
 import numpy as np
+import matplotlib as mpl
 import matplotlib.pyplot as plt
+import copy
 
 annots=  loadmat('20nm-120Vn.mat')
 # print(annots.keys())
@@ -9,5 +11,14 @@ nmatrix = annots['nmatrix']
 X = np.linspace(0, -120, 1201)
 Y = np.linspace(0, 1000, 1001)
 
-plt.contourf(X, Y, nmatrix)
+fig,ax = plt.subplots()
+
+ax.contourf(X, Y, nmatrix, vmin=0, vmax=1e25)
+
+# 自定义colorbar上下限
+cmap1 = copy.copy(mpl.cm.viridis)
+norm1 = mpl.colors.Normalize(vmin=0, vmax=1e25)
+im1 = mpl.cm.ScalarMappable(norm=norm1, cmap=cmap1)
+cbar1 = fig.colorbar(im1, orientation='vertical')
+
 plt.show()
